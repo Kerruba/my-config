8851b323ae7e4100ae4cc94fb7640d8a5f463409
Kerruba <luca.cherubin@gmail.com>
2015-11-18 09:46:34 +0000

    Add a readme

---
+++ README.mk
@@ -0,0 +1,8 @@
+# Personal VIM configuration
+This is just a repository to hold my experiments with `VIM` configuration file 
+
+Every time I will find a new command, configuration, plugin, or anything interesting to me I'll
+add it to this config.
+
+This config file is created form the ground up. There are other repositories out there with all the configurations
+already made. This is not the scope of this repository. This is just for learning purposes.
